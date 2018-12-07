278b27c36c4ca393dd6aee1c2b76896041e6191b
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-12-07 22:33:40 +0300

    Connection with lexer

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+#include "lexer/lex.yy.c"
+
+void start_lexer() {
+    FILE *in = fopen("main.go", "r");
+    FILE *out = fopen("out", "w");
+    char buffer[128];
+
+    yyset_in(in);
+    //yyset_out(out);
+    yylex();
+    fclose(in);
+    fclose(out);
+}
+
 int main() {
-    printf("Hello, World!\n");
+    start_lexer();
     return 0;
 }
